95f47347463d791ce9c463a9257bf6bf788a6235
Christian <ChrisWcs@users.noreply.github.com>
2017-11-10 12:18:16 -0700

    Setup ConSimons in Game

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import ConSimon from '../containers/ConSimon';
+
 const style = {
     row: {
         display: "flex",
@@ -18,9 +20,17 @@ const style = {
 
 const Game = () => (
     <div style={style.column}>
-        <div style={style.row}></div>
-        <div style={style.row}></div>
-        <div style={style.row}></div>
+        <div style={style.row}>
+            <ConSimon high={"#B71C1C"} norm={"#FF5252"} cNum={0} radius={"100px 0px 0px 0px"}/>
+            <ConSimon high={"#0D47A1"} norm={"#448AFF"} cNum={1} radius={"0px 100px 0px 0px"}/>
+        </div>
+        <div style={style.row}>
+            <ConSimon high={"#4A148C"} norm={"#E040FB"} cNum={3} radius={"0px 0px 0px 100px"}/>
+            <ConSimon high={"#1B5E20"} norm={"#69F0AE"} cNum={2} radius={"0px 0px 100px 0px"}/>
+        </div>
+        <div style={style.row}>
+            
+        </div>
     </div>
 );
 
